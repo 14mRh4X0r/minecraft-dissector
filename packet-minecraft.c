72bd1873efb132d6ab7cddc4a96cb8837ca83479
Willem Mulder <willemmaster@hotmail.com>
2020-12-28 16:23:48 +0100

    Add option to decrypt traffic given the server key

--- packet-minecraft.c
+++ packet-minecraft.c
@@ -1,5 +1,6 @@
 #define WS_BUILD_DLL
 
+#include <epan/expert.h>
 #include <epan/packet.h>
 #include <epan/proto.h>
 #include <epan/conversation.h>
@@ -8,6 +9,12 @@
 #include <ws_symbol_export.h>
 #include <ws_version.h>
 #include <stdbool.h>
+
+#ifdef HAVE_LIBGNUTLS
+#include <gnutls/gnutls.h>
+#include <gnutls/abstract.h>
+#include <gnutls/crypto.h>
+#endif
 
 #ifndef VERSION
 #define VERSION "0.0.0"
@@ -37,6 +44,13 @@ static int hf_data_length = -1;
 static int hf_packet_id = -1;
 static int hf_data = -1;
 
+static expert_field ei_decrypt_failed = EI_INIT;
+static expert_field ei_decrypted_data = EI_INIT;
+
+#ifdef HAVE_LIBGNUTLS
+static const char* privkey_filename;
+#endif
+
 static hf_register_info hf[] = {
 
     { &hf_length,
@@ -54,6 +68,14 @@ static hf_register_info hf[] = {
     { &hf_data,
         { "Data", "minecraft.data", FT_BYTES, BASE_NONE, NULL,
             0x0, "Packet Data", HFILL }},
+};
+
+static ei_register_info ei[] = {
+    { &ei_decrypt_failed,
+        { "minecraft.decrypt_failed", PI_DECRYPTION, PI_ERROR, "Decryption failed", EXPFILL }},
+
+    { &ei_decrypted_data,
+        { "minecraft.decrypted", PI_DECRYPTION, PI_NOTE, "Decrypted data", EXPFILL }},
 };
 
 static int ett_minecraft = -1;
@@ -138,11 +160,17 @@ void minecraft_add_buffer(proto_tree *tree, int hfindex, tvbuff_t *tvb, guint *o
 struct minecraft_conversation_data {
     guint32 state;
     guint32 compression_threshold;
+#ifdef HAVE_LIBGNUTLS
+    gnutls_privkey_t *privkey;
+    gnutls_cipher_hd_t *cipher_hd[2];
+#endif
 };
 
 struct minecraft_frame_data {
     guint32 state;
     bool compressed;
+    bool encrypted;
+    guchar* decrypted;
 };
 
 
@@ -153,20 +181,8 @@ static void dissect_minecraft_packet(tvbuff_t *tvb, packet_info *pinfo, proto_tr
     conversation_t *conversation = find_or_create_conversation(pinfo);
 
     struct minecraft_conversation_data *conversation_data = (struct minecraft_conversation_data *)conversation_get_proto_data(conversation, proto_minecraft);
-    if (conversation_data == NULL) {
-        conversation_data = wmem_new0(wmem_file_scope(), struct minecraft_conversation_data);
-        conversation_data->state = 0;
-        conversation_data->compression_threshold = 0;
-        conversation_add_proto_data(conversation, proto_minecraft, conversation_data);
-    }
 
     struct minecraft_frame_data *frame_data = (struct minecraft_frame_data  *)p_get_proto_data(wmem_file_scope(), pinfo, proto_minecraft, 0);
-    if (frame_data == NULL) {
-        frame_data = wmem_new(wmem_file_scope(), struct minecraft_frame_data);
-        frame_data->state = conversation_data->state;
-        frame_data->compressed = conversation_data->compression_threshold != 0;
-        p_add_proto_data(wmem_file_scope(), pinfo, proto_minecraft, 0, frame_data);
-    }
 
     if (frame_data->compressed) {
         const guint offset_start = offset;
@@ -213,9 +229,44 @@ static void dissect_minecraft_packet(tvbuff_t *tvb, packet_info *pinfo, proto_tr
                 status_toClient(packet_id, tvb, pinfo, subtree, offset, data_len);
             break;
         case 2:
-            if (to_server)
+            if (to_server) {
                 login_toServer(packet_id, tvb, pinfo, subtree, offset, data_len);
-            else {
+
+#ifdef HAVE_LIBGNUTLS
+                if (packet_id == 0x01 && conversation_data->privkey) {
+                    do {
+                        gnutls_datum_t ciphertext;
+                        gnutls_datum_t plaintext;
+
+                        ciphertext.size = tvb_get_ntohs(tvb, offset);
+                        ciphertext.data = tvb_memdup(wmem_packet_scope(), tvb, offset + 2, ciphertext.size);
+
+                        int res = gnutls_privkey_decrypt_data(*conversation_data->privkey, 0,
+                                                              &ciphertext, &plaintext);
+                        if (res != GNUTLS_E_SUCCESS) {
+                            proto_tree_add_expert_format(tree, pinfo, &ei_decrypt_failed, tvb, offset + 2, ciphertext.size,
+                                                         "Failed to decrypt shared secret: %s", gnutls_strerror(res));
+                            break;
+                        }
+
+                        conversation_data->cipher_hd[false] = wmem_new(wmem_file_scope(), gnutls_cipher_hd_t);
+                        if (gnutls_cipher_init(conversation_data->cipher_hd[false], GNUTLS_CIPHER_AES_128_CFB8,
+                                               &plaintext, &plaintext) != GNUTLS_E_SUCCESS) {
+                            conversation_data->cipher_hd[false] = NULL;
+                            break;
+                        }
+
+                        conversation_data->cipher_hd[true] = wmem_new(wmem_file_scope(), gnutls_cipher_hd_t);
+                        if (gnutls_cipher_init(conversation_data->cipher_hd[true], GNUTLS_CIPHER_AES_128_CFB8,
+                                               &plaintext, &plaintext) != GNUTLS_E_SUCCESS) {
+                            conversation_data->cipher_hd[false] = NULL;
+                            conversation_data->cipher_hd[true] = NULL;
+                            break;
+                        }
+                    } while (false);
+                }
+#endif
+            } else {
                 login_toClient(packet_id, tvb, pinfo, subtree, offset, data_len);
 
                 switch (packet_id) {
@@ -240,7 +291,72 @@ static void dissect_minecraft_packet(tvbuff_t *tvb, packet_info *pinfo, proto_tr
 static int dissect_minecraft(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree, void *data _U_)
 {
     guint offset = 0;
-    while (offset < tvb_reported_length(tvb)) {
+    bool to_server = pinfo->destport == pinfo->match_uint;
+
+    conversation_t *conversation = find_or_create_conversation(pinfo);
+    struct minecraft_conversation_data *conversation_data = (struct minecraft_conversation_data *)conversation_get_proto_data(conversation, proto_minecraft);
+
+
+    if (conversation_data == NULL) {
+        conversation_data = wmem_new0(wmem_file_scope(), struct minecraft_conversation_data);
+
+#ifdef HAVE_LIBGNUTLS
+        if (privkey_filename) {
+            do {
+                gnutls_datum_t privkey_data;
+                if (gnutls_load_file(privkey_filename, &privkey_data) != GNUTLS_E_SUCCESS) {
+                    break;
+                }
+
+                conversation_data->privkey = wmem_new(wmem_file_scope(), gnutls_privkey_t);
+                if (gnutls_privkey_init(conversation_data->privkey) != GNUTLS_E_SUCCESS) {
+                    conversation_data->privkey = NULL;
+                    break;
+                }
+
+                if (gnutls_privkey_import_x509_raw(*conversation_data->privkey, &privkey_data, GNUTLS_X509_FMT_DER,
+                                                   NULL, GNUTLS_PKCS_PLAIN) != GNUTLS_E_SUCCESS) {
+                    conversation_data->privkey = NULL;
+                    break;
+                }
+            } while (false);
+        }
+#endif
+
+        conversation_add_proto_data(conversation, proto_minecraft, conversation_data);
+    }
+
+    struct minecraft_frame_data *frame_data = (struct minecraft_frame_data  *)p_get_proto_data(wmem_file_scope(), pinfo, proto_minecraft, 0);
+
+    if (frame_data == NULL) {
+        frame_data = wmem_new(wmem_file_scope(), struct minecraft_frame_data);
+        frame_data->state = conversation_data->state;
+        frame_data->compressed = conversation_data->compression_threshold != 0;
+        frame_data->encrypted = conversation_data->cipher_hd[to_server] != NULL;
+        p_add_proto_data(wmem_file_scope(), pinfo, proto_minecraft, 0, frame_data);
+    }
+
+    guint reported_length = tvb_reported_length(tvb);
+
+#ifdef HAVE_LIBGNUTLS
+    if (frame_data->encrypted) {
+        if (frame_data->decrypted == NULL) {
+            do {
+                frame_data->decrypted = tvb_memdup(wmem_file_scope(), tvb, offset, reported_length);
+                int err = gnutls_cipher_decrypt(*conversation_data->cipher_hd[to_server], frame_data->decrypted, reported_length);
+                if (err != GNUTLS_E_SUCCESS) {
+                    expert_add_info_format(pinfo, NULL, &ei_decrypt_failed, "Decrypt failed: %s",
+                                           gnutls_strerror(err));
+                    break;
+                }
+            } while (false);
+        }
+
+        tvb = tvb_new_child_real_data(tvb, frame_data->decrypted, reported_length, reported_length);
+    }
+#endif
+
+    while (offset < reported_length) {
         guint available = tvb_reported_length_remaining(tvb, offset);
         guint32 len = 0;
         const guint offset_start = offset;
@@ -262,6 +378,11 @@ static int dissect_minecraft(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree
 
         proto_tree *subtree = proto_item_add_subtree(item, ett_minecraft);
 
+        if (frame_data->encrypted) {
+            proto_tree *expert_info = proto_tree_add_expert(subtree, pinfo, &ei_decrypted_data, tvb, offset, len);
+            proto_tree_add_bytes(expert_info, hf_data, tvb, offset, len, frame_data->decrypted);
+        }
+
         proto_tree_add_uint(subtree, hf_length, tvb, offset_start, offset - offset_start, len);
 
         dissect_minecraft_packet(tvb, pinfo, subtree, offset, len);
@@ -279,6 +400,15 @@ static void proto_register_minecraft(void)
     proto_register_field_array(proto_minecraft, hf, array_length(hf));
     proto_register_field_array(proto_minecraft, hf_generated, array_length(hf_generated));
     proto_register_subtree_array(ett, array_length(ett));
+
+#ifdef HAVE_LIBGNUTLS
+    module_t *minecraft_module = prefs_register_protocol(proto_minecraft, NULL);
+    prefs_register_filename_preference(minecraft_module, "privkey_file", "Private key file",
+                                       "The file containing the server's private key in DER format", &privkey_filename, FALSE);
+#endif
+
+    expert_module_t *expert_module = expert_register_protocol(proto_minecraft);
+    expert_register_field_array(expert_module, ei, array_length(ei));
 }
 
 static void proto_reg_handoff_minecraft(void)
